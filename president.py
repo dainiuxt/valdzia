9a8f9f4167748cd46345383e84e503ba60bc123c
dainiuxt <dainiuxt@gmail.com>
2014-10-24 22:01:20 +0300

    commit

--- president.py
+++ president.py
@@ -1,10 +1,17 @@
 #scrap <p class="vv"> (Name) and <p class="vvv"> (position)
+from lxml import html
 import requests
 import bs4
 
 #get contacts page
-pres_response = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
-presfile = open('president.txt', 'w')
+presidentr = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
+presidenttree = html.fromstring(presidentr.text)
+pr_names = presidenttree.xpath('//p[@class="vv"]/text()')
+pr_position = presidenttree.xpath('//p[@class="vvv"]/text()')
+
+print (pr_names)
+print (pr_position)
+#presidentf = open('president.txt', 'w')
 #presfile.encode('utf-8')
-presfile.write(pres_response.text)
-presfile.close
+#presidentf.write(presidentr.text)
+#presidentf.close
