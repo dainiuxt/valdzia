f4d543ef022c9c5b075286062dccf63948b2dd47
dainiuxt <dainiuxt@gmail.com>
2014-10-24 20:52:52 +0300

    commit

--- president.py
+++ president.py
@@ -1,8 +1,10 @@
 #scrap <p class="vv"> (Name) and <p class="vvv"> (position)
 import requests
-import BeautifulSoup
+import bs4
 
 #get contacts page
-president_response = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
-
-print president_response.text
+pres_response = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
+presfile = open('president.txt', 'w')
+#presfile.encode('utf-8')
+presfile.write(pres_response.text)
+presfile.close
