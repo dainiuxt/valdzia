5994f83736e7915a8e1866379ec5f51d0d52a455
dainiuxt <dainiuxt@gmail.com>
2014-10-24 19:43:45 +0300

    first working version

--- president.py
+++ president.py
@@ -1,6 +1,6 @@
 #scrap <p class="vv"> (Name) and <p class="vvv"> (position)
 import requests
-import bs4
+import BeautifulSoup
 
 #get contacts page
 president_response = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
