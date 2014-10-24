dd7feee20ae658c22e6550988dfd8fd77839b2aa
dainiuxt <dainiuxt@gmail.com>
2014-10-24 06:53:07 +0300

    start

---
+++ president.py
@@ -0,0 +1,8 @@
+#scrap <p class="vv"> (Name) and <p class="vvv"> (position)
+import requests
+import bs4
+
+#get contacts page
+president_response = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
+
+print president_response.text
