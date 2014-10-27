4066768c3144634d95decd2a26d739fbe0040373
dainiuxt <dainiuxt@gmail.com>
2014-10-27 06:47:13 +0200

    commit

--- president.py
+++ president.py
@@ -2,15 +2,21 @@
 from lxml import html
 import requests
 import bs4
+from bs4 import BeautifulSoup
 
-#get contacts page
-presidentr = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
-presidenttree = html.fromstring(presidentr.text)
-pr_names = presidenttree.xpath('//p[@class="vv"]/text()')
-pr_position = presidenttree.xpath('//p[@class="vvv"]/text()')
+president_data = {}
+president_url = 'http://president.lt/lt/dbs_kontaktai/printerlist.html'
+president_read = requests.get(president_url) #response
+president_soup = bs4.BeautifulSoup(president_read.text)
+president_data['Vardas'] = president_soup.select('p.vv')#.get.text()
+president_data['Pareigos'] = president_soup.select('p.vvv')#.get.text()
+#president_tree = html.fromstring(president_read.text)
+#president_names = president_tree.xpath('//p[@class="vv"]/text()')
+#president_position = president_tree.xpath('//p[@class="vvv"]/text()')
+#president_names.extend(president_position)
+print (president_data)
 
-print (pr_names)
-print (pr_position)
+	
 #presidentf = open('president.txt', 'w')
 #presfile.encode('utf-8')
 #presidentf.write(presidentr.text)
