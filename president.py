768ec842222a008b5b6393ad5c02141f644e3fd2
dainiuxt <dainiuxt@gmail.com>
2014-10-30 06:38:20 +0200

    for i in range(len(get_pr_contacts_data)): TypeError: object of type 'function' has no len()

--- president.py
+++ president.py
@@ -1,16 +1,31 @@
+# create virtual python3  environment:
+# source py3env/bin/activate
+
 import re
 import argparse
-from lxml import html
 import requests
 import bs4
-from bs4 import BeautifulSoup
+from multiprocessing.pool import ThreadPool as Pool
 from time import sleep
 
 pr_url = 'http://president.lt/lt/dbs_kontaktai/printerlist.html'
-pr_data = {}
-pr_read = requests.get(pr_url)
-pr_soup = bs4.BeautifulSoup(pr_read.text)
-pr_data['Vardas'] = pr_soup.select('p.vv')[0].get_text()
-pr_data['Pareigos'] = pr_soup.select('p.vvv')[0].get_text()
-print (pr_data)
-sleep(1)
+
+#get contacts data from president.lt
+def get_pr_contacts_data():
+	pr_data = {}
+	pr_response = requests.get(pr_url)
+	pr_soup = bs4.BeautifulSoup(pr_response.text)
+	pr_data['Vardas'] = pr_soup.selectAll('p.vv')[0].get_text()
+	pr_data['Pareigos'] = pr_soup.selectAll('p.vvv')[0].get_text()
+	return pr_data
+	sleep(1)
+	
+#print (pr_data)
+def show_pr_data():
+	results = get_pr_contacts_data
+	print (results)
+	print(u'Vardas, Pareigos')
+	print(u'{0},{1}'.format(results[i]['Vardas'], ', '.join(results[i]['Pareigos'])))
+
+if __name__ == '__main__':
+    show_pr_data()
