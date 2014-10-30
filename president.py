49668b935ef0dcfae47a223de8f50868571717ed
dainiuxt <dainiuxt@gmail.com>
2014-10-30 22:08:28 +0200

    Added "Hack with Nitrous.io" functionality, merged lists

--- president.py
+++ president.py
@@ -9,17 +9,18 @@ presidentr = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html
 presidenttree = html.fromstring(presidentr.text)
 pr_names = presidenttree.xpath('//p[@class="vv"]/text()')
 pr_position = presidenttree.xpath('//p[@class="vvv"]/text()')
-sleep(1)
+#~ sleep(1)
 
-def extract_pr_staff(pr_names, pr_position):
+def extract_pr_staff():
 	pr_staff = []
-	maxnum = len(pr_names)
-	for i in range(maxnum):
-		pr_staff.append(zip(pr_names[i], pr_position[i]))
-	return pr_staff
-	print(pr_staff)
-
-if __name__ == '__main__':
-    extract_pr_staff(pr_names, pr_position)
-#~ print (pr_names)
-#~ print (pr_position)
+	print(len(pr_names))
+	max = len(pr_names)
+	for i in range(max):
+		return pr_staff.append(zip(pr_names[i], pr_position[i]))
+	#~ return pr_staff
+	
+extract_pr_staff()
+#~ if __name__ == '__main__':
+    #~ extract_pr_staff(pr_names, pr_position)
+#~ print (zip(pr_names, pr_position))
+#~ print (extract_pr_staff)
