efd465d7e142a90eaf8324a4e6015330cdc3d2af
dainiuxt <dainiuxt@gmail.com>
2014-10-31 20:23:53 +0200

    Updated Readme.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-valdzia
+Valdžia [Government]
 =======
 
 [![Hack dainiuxt/valdzia on Nitrous.IO](https://d3o0mnbgv6k92a.cloudfront.net/assets/hack-l-v1-4b6757c3247e3c50314390ece34cdb11.png)](https://www.nitrous.io/hack_button?source=embed&runtime=django&repo=dainiuxt%2Fvaldzia)
--- president.py
+++ president.py
@@ -5,20 +5,24 @@ import bs4
 from time import sleep # be nice
 
 #get contacts page
+pr_names = []
+pr_position = []
 presidentr = requests.get('http://president.lt/lt/dbs_kontaktai/printerlist.html')
 presidenttree = html.fromstring(presidentr.text)
-pr_names = presidenttree.xpath('//p[@class="vv"]/text()')
-pr_position = presidenttree.xpath('//p[@class="vvv"]/text()')
+pr_names.append(presidenttree.xpath('//p[@class="vv"]/text()'))
+pr_position.append(presidenttree.xpath('//p[@class="vvv"]/text()'))
 #~ sleep(1)
+pr_staff = list(zip(pr_names, pr_position))
+#~ print(pr_staff)
 
 def extract_pr_staff():
 	pr_staff = []
 	print(len(pr_names))
 	max = len(pr_names)
 	for i in range(max):
-		return pr_staff.append(zip(pr_names[i], pr_position[i]))
-	#~ return pr_staff
-	
+		return pr_staff.append(list(zip(pr_names[i], pr_position[i]))) 
+	return pr_staff
+	#~ 
 extract_pr_staff()
 #~ if __name__ == '__main__':
     #~ extract_pr_staff(pr_names, pr_position)
